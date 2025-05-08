c1e8eec73bbdc378c14cb635cb8f7c91f0cb0b04
Ohyoo <89747528+Ohyoo-Labs@users.noreply.github.com>
2025-05-08 17:03:36 -0300

    Update dictionary.js

--- @NativeEncrypt/dictionary.js
+++ @NativeEncrypt/dictionary.js
@@ -1,3 +1,3 @@
 // Diccionario de caracteres con signos especiales y todos los caracteres permitido para un texto en español
 export const dictionary =
-  "aábcdeéfghiíjklmnñoópqrstuúüvwxyzABCDEFGHIJKLMNÑOPQRSTUÜVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>\"'? ";
+  "aábcçdeéfghiíjklmnñoópqrstuúüvwxyzABCDEFGHIJKLMNÑOPQRSTUÜVWXYZ0123456789¡!@#$%^&*()_+-=[]{}|;:,.\\<>\"'¿? ";
